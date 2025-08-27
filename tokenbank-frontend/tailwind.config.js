124d98d42c72f991b55c5c80cfd1605210bbe308
Adetayo <twinebravo@gmail.com>
2025-08-27 12:56:41 +0100

    FPostCSS plugin for Tailwind CSS issue fixed - vite.config

--- tokenbank-frontend/tailwind.config.js
+++ tokenbank-frontend/tailwind.config.js
@@ -2,10 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Ensure this covers all TS/TSX files
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
--- tokenbank-frontend/vite.config.ts
+++ tokenbank-frontend/vite.config.ts
@@ -1,15 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import postcss from 'postcss';
-import tailwindcss from '@tailwindcss/postcss';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: {
-      plugins: [
-        tailwindcss(),
-      ],
-    },
+    postcss: './postcss.config.js',
   },
-});
+})
